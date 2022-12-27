16954fd85c39873a728c042cc54c2e95061e5045
arianitxyz <arianitxyz@hotmail.com>
2022-12-27 14:03:47 +0100

    ->206

---
+++ Section_7_Dictionaries_and_Sets/shallow_copy.py
@@ -0,0 +1,15 @@
+animals = {
+    "lion": ["scary", "big", "cat"],
+    "elephant": ["big", "grey", "wrinkled"],
+    "teddy": ["cuddly", "stuffed"],
+}
+
+things = animals.copy()
+# animals["teddy"] = "toy"
+print(things["teddy"])
+print(animals["teddy"])
+
+print()
+things["teddy"].append("toy")
+print(things["teddy"])
+print(animals["teddy"])
