cd9fdb72c4d96ca1cc5bac61018132d81bcbe3af
arianitxyz <arianitxyz@hotmail.com>
2023-01-07 16:13:12 +0100

    -> 261

--- TextFiles/read_poem.py
+++ TextFiles/read_poem.py
@@ -1,38 +1,4 @@
-# jabber = open('Jabberwocky.txt', 'r')
-#
-# for line in jabber:
-#     print(line.rstrip())
-#     # print(len(line))
-#
-# jabber.close()
 
-# with open('Jabberwocky.txt', 'r') as jabber:
-#     # for line in jabber:
-#     #     print(line.rstrip())
-#     lines = jabber.readlines()
-# print(lines)
-# print(lines[-1:])
-
-# print text
-
-# with open('Jabberwocky.txt','r') as jabber:
-#     text = jabber.read()
-#
-# for character in reversed(text):
-#     print(character, end='')
-#
-
-with open('Jabberwocky.txt', 'r') as jabber:
-    while True:
-        line = jabber.readline().rstrip()
-        print(line)
-        if 'jubjub' in line.casefold():
-            break
-
-print('*' * 80)
-
-with open('Jabberwocky.txt') as jabber:
+with open('Jabberwocky.txt', encoding='utf-8') as jabber:
     for line in jabber:
         print(line.rstrip())
-        if 'jubjub' in line.casefold():
-            break
